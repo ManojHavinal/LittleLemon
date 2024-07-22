c33bea0524f6a2f87273b4ba59da3be672fca426
ManojHavinal <144484792+ManojHavinal@users.noreply.github.com>
2024-07-22 16:27:08 +0530

    LittleLemon App

--- App/restaurant/admin.py
+++ App/restaurant/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from . models import Booking_table,Menu_table
+admin.site.register(Booking_table)
+admin.site.register(Menu_table)
 # Register your models here.
--- App/restaurant/models.py
+++ App/restaurant/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Booking_table(models.Model):
+    Name = models.CharField(max_length=225)
+    no_of_guests = models.IntegerField(default=0)
+    Booking_date = models.DateTimeField(default=timezone.now)  # Set default value here
+
+
+class Menu_table(models.Model):
+    title = models.CharField(max_length=225)
+    price = models.DecimalField(max_digits=10, decimal_places=2,default=0)
+    Inventory = models.IntegerField(default=0)              # Changed 'Inventory' to 'inventory' for consistency
+
+
